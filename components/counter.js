73eb172b9a06a9ece0892d8b63ce98f7000d6125
Kellen <142949567+Hurlicane24@users.noreply.github.com>
2024-11-10 20:51:37 -0700

    Update counter.js

--- components/counter.js
+++ components/counter.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({step, buttonColor}) {
     const [count, setCount] = useState(0);
-  
+
+    const reset = () => {
+        setCount((previous) => {
+            const New = previous + step;
+            if(New > 10) {
+                return(0);
+            }
+            else {
+                return(New);
+            }
+        })
+    }
+
     return (
       <div>
         <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={reset} style={{backgroundColor: buttonColor}}>Increment</button>
       </div>
     );
-  }
+}
