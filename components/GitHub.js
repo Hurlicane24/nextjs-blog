445979fa71be445d029e465c005549e55091a29a
Kellen <142949567+Hurlicane24@users.noreply.github.com>
2024-11-10 20:53:23 -0700

    Create GitHub.js

---
+++ components/GitHub.js
@@ -0,0 +1,11 @@
+export default function GitHub() {
+
+    return (
+        <div>
+            <p>I want to do my final project on APIs. In particular, I will use the Spotify API to implement
+                a true shuffle algorithm rather than Spotify's default shuffle algorithm.
+            </p>
+            <p>Here's my <a href="https://github.com/Hurlicane24/nextjs-blog"> repo for this homework project</a></p>
+        </div>
+    );
+}
